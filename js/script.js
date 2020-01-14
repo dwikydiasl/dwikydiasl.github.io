a70b596b1a5e2fa1fbdd57e9b91a7fc67d555e22
dwiky dias <diaslaksmana@gmail.com>
2020-01-14 09:24:47 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-//loading page
-function onReady(callback) {
-  var intervalID = window.setInterval(checkReady, 3000);
-  function checkReady() {
-    if (document.getElementsByTagName('body')[0] !== undefined) {
-      window.clearInterval(intervalID);
-      callback.call(this);
+// Section
+var sections = $('section')
+, nav = $('nav')
+, nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+    bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
     }
-  }
-}
-function show(id, value) {
-  document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-onReady(function () {
-  show('header', true);
-  show('home', true);
-  show('about', true);
-  show('service', true);
-  show('portofolio', true);
-  show('info', true);
-  show('loading', false);
+  });
 });
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+  , id = $el.attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 700);
+
+  return false;
+});
+
+
 
 //
 
@@ -45,6 +57,69 @@ $(function() {
   });
   
 });
+
+//transition 1
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'img-hero-fade'
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
+
+//transition 2
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'fade-right-content'
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
+
 
 //button
 $(".hover").mouseleave(
@@ -73,3 +148,37 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
+//parallax 2
+(function() {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax2");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
+
+})();
+
+//
+$('.fabs').click(function(){
+  $('.fab-options').toggle('slow');
+  $('.fabs i').toggleClass('fa-times fa-plus');
+});
+
+//
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
