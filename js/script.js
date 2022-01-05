dc7cf633599edc230d696d9abc69b2d95752760a
dias laksmana <dias@softwareseni.com>
2022-01-05 11:31:50 +0700

    update

--- js/script.js
+++ js/script.js
@@ -142,31 +142,4 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
-// animated fade in scroll
-(function() {
-  var elements;
-  var windowHeight;
-
-  function init() {
-    elements = document.querySelectorAll('.hidden');
-    windowHeight = window.innerHeight;
-  }
-
-  function checkPosition() {
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      var positionFromTop = elements[i].getBoundingClientRect().top;
-
-      if (positionFromTop - windowHeight <= 0) {
-        element.classList.add('fade-in-element');
-        element.classList.remove('hidden');
-      }
-    }
-  }
-
-  window.addEventListener('scroll', checkPosition);
-  window.addEventListener('resize', init);
-
-  init();
-  checkPosition();
-})();
+// cursor
